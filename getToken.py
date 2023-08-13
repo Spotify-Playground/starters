ec323985571c317b51e904ac5d3306ec9d26573c
iamyanghee <yanghee.data@gmail.com>
2023-08-13 12:58:08 +0900

    주석 등 제거

--- getToken.py
+++ getToken.py
@@ -4,8 +4,6 @@ import config.client as client
 CLIENT_ID = client.ID
 CLIENT_SECRETS = client.SECRETS
 
-# API 엔드포인트 URL
-# url = "https://example.com/api_endpoint"
 url = "https://accounts.spotify.com/api/token"
 headers = {
     "Content-Type": "application/x-www-form-urlencoded"
@@ -16,21 +14,15 @@ data = {
     "client_secret": CLIENT_SECRETS
 }
 
-# 요청 보내고 응답 받기
-# response = requests.get(url)
 response = requests.post(url, headers=headers, data=data)
 
-# 응답 JSON 데이터 디코딩
 if response.status_code == 200:  # HTTP OK
     response_json = response.json()
-    print("Received JSON response:")
-    print(response_json)
 else:
     print(f"Error: {response.status_code}")
 
-# JSON 응답에서 원하는 정보 추출 및 사용
 if "access_token" in response_json:
     access_token = response_json["access_token"]
-    print(f"Value of 'key': {access_token}")
+    print(f"Value of 'access_token': {access_token}")
 else:
-    print("Key 'key' not found in the JSON response.")
+    print("Key 'access_token' not found in the JSON response.")
